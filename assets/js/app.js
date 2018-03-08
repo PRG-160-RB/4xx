3463ac2106ffb853b32729df600079eb6888dac4
RobertBright <BobbyB1967@gmail.com>
2018-03-07 23:56:29 -0500

    added progress bar, displayPB function, removed application tagline var in app.js also added cdn boostrap link in the index.html file

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@
 
 
 
-var applicationTagline = 'continously falling forward into the light';
+
 
 var appData = {
 	title: 'inside out - js',
@@ -42,7 +42,7 @@ var elWrapper = document.createElement('div');
     
     
     var elMain = document.createElement('main');
-    elMain.innerHTML = '<p> Hello World! </p>'
+    elMain.innerHTML = '<p> Hello World! </p>';
     elWrapper.appendChild(elMain);
 	 elMain.className = 'animated zoomIn';
     
@@ -50,10 +50,40 @@ var elWrapper = document.createElement('div');
      elFooter.innerHTML = '<h4>get excited about learning JavaScript...</h4>';
     elWrapper.appendChild(elFooter);
     elFooter.className = 'animated bounceInRight';
-    
-    
-       
-        
-    
-    
+	
+	elMain.innerHTML += '<div style="width: 40%; margin: auto; padding:10px;margin-bottom: 20px;"><div class="progress" style="height: 20px;"><div id="loaderProgressBar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div></div></div>';
+   displayPB();
 }
+
+var timerCount = 0;
+function displayPB() {
+    if (timerCount <= 100) {
+        var x = (timerCount < 25) ? '' : (timerCount < 45) ? timerCount + '%' : (timerCount < 65) ? 'Loading ' + timerCount + '%' : 'Loading Application ' + timerCount + '%';
+        document.getElementById("loaderProgressBar").innerHTML = x;
+        document.getElementById('loaderProgressBar').setAttribute('aria-valuenow', timerCount);
+        document.getElementById('loaderProgressBar').style.width = timerCount + '%    ';
+        timerCount++;
+        setTimeout(displayPB, 50);
+    } else {
+        timerCount = 0;
+        
+       
+
+        return false;
+    }
+}
+    
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
